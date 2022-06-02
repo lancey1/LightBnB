fef62e817b72227e79ac5145e293747913d591b0
vagrant <lanceyu619@gmail.com>
2022-06-02 20:27:18 +0000

    refactor getAllreservations

--- Lightbnb_app/server/database.js
+++ Lightbnb_app/server/database.js
@@ -81,9 +81,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT properties.*, reservations.*, AVG(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN reservations ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 AND reservations.end_date > now()
+    GROUP BY properties.id, reservations.id
+    ORDER BY start_date ASC
+    LIMIT $2;`, [guest_id,limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
-
 /// Properties
 
 /**
